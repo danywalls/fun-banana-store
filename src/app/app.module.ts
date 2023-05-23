3814ef99cbe1f3ed32c15dc2947da5defb4158bc
Dany Paredes <danywalls4@gmail.com>
2023-05-23 20:28:19 +0200

    using ngx-banana-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxBananaUiModule } from 'ngx-banana-ui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxBananaUiModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
